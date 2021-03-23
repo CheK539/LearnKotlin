aa1a5476949bc0c570e686e11147cd0071fde231
Ivan <chek539camo@gmail.com>
2021-03-24 01:17:34 +0500

    Added selected color functional at DisplayFormActivity.kt and color scroll view with rectangle at activity_display_form.xml, deleted background images

--- app/src/main/java/com/example/learnkotlin/DisplayFormActivity.kt
+++ app/src/main/java/com/example/learnkotlin/DisplayFormActivity.kt
@@ -1,16 +1,19 @@
 package com.example.learnkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.RadioButton
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.drawToBitmap
 import androidx.databinding.DataBindingUtil
 import com.example.learnkotlin.databinding.ActivityDisplayFormBinding
 import com.example.learnkotlin.habitModel.HabitElement
+
 
 class DisplayFormActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private lateinit var binding: ActivityDisplayFormBinding
@@ -38,22 +41,56 @@ class DisplayFormActivity : AppCompatActivity(), AdapterView.OnItemSelectedListe
             isNewHabit = false
             fillEditForm(position)
         }
+
+        addGlobalColorLayoutListener()
+    }
+
+    private fun addGlobalColorLayoutListener() =
+        binding.colorsLayout.viewTreeObserver.addOnGlobalLayoutListener(
+            object : OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    binding.colorsLayout.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    fillButtonColors()
+                }
+            }
+        )
+
+    private fun fillButtonColors() {
+        val childCount = binding.colorsLayout.childCount
+        val bitmap = binding.colorsLayout.drawToBitmap()
+
+        for (i in 0 until childCount) {
+            val view = binding.colorsLayout.getChildAt(i)
+            val (x, y) = getCenterPosition(view, binding.colorsLayout)
+            val pixel = bitmap.getPixel(x, y)
+            view.setBackgroundColor(pixel)
+        }
+    }
+
+    private fun getCenterPosition(view: View, layout: LinearLayout): Pair<Int, Int> {
+        val rectanglePosition = IntArray(2)
+        val layoutPosition = IntArray(2)
+        view.getLocationOnScreen(rectanglePosition)
+        layout.getLocationOnScreen(layoutPosition)
+        val x = rectanglePosition[0] - layoutPosition[0] + view.width / 2
+        val y = rectanglePosition[1] - layoutPosition[1] + view.height / 2
+        return Pair(x, y)
     }
 
     private fun fillEditForm(position: Int) {
         val habit = habitElements[position]
         binding.titleEditText.setText(habit.title)
         binding.descriptionEditText.setText(habit.description)
-
-        arrayAdapter.apply { binding.prioritySpinner.setSelection(getPosition(habit.priority)) }
-
         binding.completeCounter.setText(habit.completeCounter)
         binding.periodNumber.setText(habit.periodNumber)
+        binding.colorButton.setBackgroundColor(Color.parseColor(habit.color.split(" ")[0]))
         binding.typesGroup.check(
             if (habit.type == binding.negativeType.text.toString())
                 R.id.negativeType
             else R.id.positiveType
         )
+
+        arrayAdapter.apply { binding.prioritySpinner.setSelection(getPosition(habit.priority)) }
     }
 
     private fun validateForm(): Boolean {
@@ -65,16 +102,22 @@ class DisplayFormActivity : AppCompatActivity(), AdapterView.OnItemSelectedListe
         return true
     }
 
-    private fun createHabit(): HabitElement =
-        HabitElement(
+    private fun createHabit(): HabitElement {
+        val rgb = (binding.colorButton.background as ColorDrawable).color
+        val hsv = FloatArray(3)
+        Color.colorToHSV(rgb, hsv)
+        val hexColor = "#${"%x".format(rgb)}"
+
+        return HabitElement(
             binding.titleEditText.text.toString(),
             binding.descriptionEditText.text.toString(),
             priorityChoice,
             findViewById<RadioButton>(binding.typesGroup.checkedRadioButtonId).text.toString(),
             binding.completeCounter.text.toString(),
             binding.periodNumber.text.toString(),
-            "None"
+            "$hexColor ${hsv[0]} ${hsv[1]} ${hsv[2]}"
         )
+    }
 
     private fun changeHabit() {
         if (!validateForm())
@@ -92,10 +135,13 @@ class DisplayFormActivity : AppCompatActivity(), AdapterView.OnItemSelectedListe
         finish()
     }
 
-    private fun deleteHabit(){
+    private fun deleteHabit() {
         habitElements.removeAt(position)
         finish()
     }
+
+    fun changeColor(view: View) =
+        binding.colorButton.setBackgroundColor((view.background as ColorDrawable).color)
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         priorityChoice = parent?.getItemAtPosition(position).toString()
