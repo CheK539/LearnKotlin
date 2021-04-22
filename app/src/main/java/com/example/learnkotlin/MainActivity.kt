d8b9ef8d519958b40e84c31776d78813ed91127b
Ivan <chek539camo@gmail.com>
2021-04-22 16:36:54 +0500

    Refactored connection between navigation drawer items.

--- app/src/main/java/com/example/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlin/MainActivity.kt
@@ -1,46 +1,46 @@
 package com.example.learnkotlin
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.learnkotlin.databinding.ActivityMainBinding
 import com.example.learnkotlin.interfaces.IDisplayFormCallback
 import com.example.learnkotlin.models.HabitElement
 import com.example.learnkotlin.viewModels.HabitsViewModel
-import com.google.android.material.navigation.NavigationView
 
 
 const val ARGS_HABIT_ELEMENTS = "habitElements"
 const val ARGS_HABIT_ELEMENT = "habitElement"
 
-class MainActivity : AppCompatActivity(), IDisplayFormCallback,
-    NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), IDisplayFormCallback {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navigation: NavController
     private lateinit var habitsViewModel: HabitsViewModel
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        if (savedInstanceState == null)
-            binding.navigationView.setCheckedItem(R.id.home_page)
 
         changeActivity()
     }
 
     private fun changeActivity() {
         setSupportActionBar(binding.toolbar)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homePage, R.id.aboutPage), binding.drawerLayout
+        )
         navigation = Navigation.findNavController(this, R.id.mainControllerContext)
-        NavigationUI.setupActionBarWithNavController(this, navigation, binding.drawerLayout)
+        NavigationUI.setupActionBarWithNavController(this, navigation, appBarConfiguration)
         NavigationUI.setupWithNavController(binding.navigationView, navigation)
-        binding.navigationView.setNavigationItemSelectedListener(this)
 
         habitsViewModel = ViewModelProvider(this).get(HabitsViewModel::class.java)
     }
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity(), IDisplayFormCallback,
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navigation, binding.drawerLayout)
+        return NavigationUI.navigateUp(navigation, appBarConfiguration)
     }
 
     override fun onBackPressed() {
@@ -68,16 +68,5 @@ class MainActivity : AppCompatActivity(), IDisplayFormCallback,
             binding.drawerLayout.closeDrawer(GravityCompat.START)
         else
             super.onBackPressed()
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.home_page -> FragmentController.openMainFragment(this)
-
-            R.id.about_page -> FragmentController.openAboutFragment(this)
-        }
-
-        binding.drawerLayout.closeDrawer(GravityCompat.START)
-        return true
     }
 }
