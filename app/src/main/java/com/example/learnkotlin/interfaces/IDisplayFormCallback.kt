91abf21261a5843055b00b342bc95e121a150f39
Ivan <chek539camo@gmail.com>
2021-04-25 00:30:45 +0500

    Deleted IDisplayFormCallback.kt.

--- app/src/main/java/com/example/learnkotlin/interfaces/IDisplayFormCallback.kt
+++
@@ -1,9 +0,0 @@
-package com.example.learnkotlin.interfaces
-
-import com.example.learnkotlin.models.HabitElement
-
-interface IDisplayFormCallback {
-    fun addHabit(habitElement: HabitElement)
-    fun deleteHabit(habitElement: HabitElement)
-    fun habitChanged()
-}
