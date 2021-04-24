5583e3530da416fce35c4c4c77e6ebe1b252672c
Ivan <chek539camo@gmail.com>
2021-04-25 02:42:03 +0500

    Changed ArrayList arguments for HabitType.

--- app/src/main/java/com/example/learnkotlin/adapters/FragmentAdapter.kt
+++ app/src/main/java/com/example/learnkotlin/adapters/FragmentAdapter.kt
@@ -4,21 +4,21 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.learnkotlin.HabitListFragment
-import com.example.learnkotlin.models.HabitElement
+import com.example.learnkotlin.HabitsListFragment
+import com.example.learnkotlin.enums.HabitType
 
 class FragmentAdapter(
     fragmentManager: FragmentManager,
     lifecycle: Lifecycle,
-    private val positiveHabits: ArrayList<HabitElement>,
-    private val negativeHabits: ArrayList<HabitElement>
+    private val firstHabitType: HabitType,
+    private val secondHabitType: HabitType
 ) : FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            1 -> HabitListFragment.newInstance(negativeHabits)
-            else -> HabitListFragment.newInstance(positiveHabits)
+            1 -> HabitsListFragment.newInstance(secondHabitType)
+            else -> HabitsListFragment.newInstance(firstHabitType)
         }
     }
 }
