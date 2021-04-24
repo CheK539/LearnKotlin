79ccfdb566b4fb28112888abb42044c3f1b3c125
Ivan <chek539camo@gmail.com>
2021-04-25 00:30:17 +0500

    Changed activity parameter for NavController

--- app/src/main/java/com/example/learnkotlin/FragmentController.kt
+++ app/src/main/java/com/example/learnkotlin/FragmentController.kt
@@ -1,32 +1,27 @@
 package com.example.learnkotlin
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.NavOptions
-import androidx.navigation.Navigation
 import com.example.learnkotlin.models.HabitElement
 
 object FragmentController {
-    fun openDisplayFormFragment(activity: AppCompatActivity, habitElement: HabitElement?) {
-        val navigation = Navigation.findNavController(activity, R.id.mainControllerContext)
-        navigation.navigate(R.id.action_homePage_to_displayFormPage, Bundle().apply {
+    fun openDisplayFormFragment(navController: NavController, habitElement: HabitElement?) {
+        navController.navigate(R.id.action_homePage_to_displayFormPage, Bundle().apply {
             putParcelable(ARGS_HABIT_ELEMENT, habitElement)
         })
     }
 
-    fun openMainFragment(activity: AppCompatActivity) {
+    fun openMainFragment(navController: NavController) {
         val navOption = NavOptions.Builder().setPopUpTo(R.id.main_controller, true).build()
-        val navigation = Navigation.findNavController(activity, R.id.mainControllerContext)
-        navigation.navigate(R.id.homePage, null, navOption)
+        navController.navigate(R.id.homePage, null, navOption)
     }
 
-    fun backToMainFragment(activity: AppCompatActivity) {
-        val navigation = Navigation.findNavController(activity, R.id.mainControllerContext)
-        navigation.popBackStack()
+    fun backToMainFragment(navController: NavController) {
+        navController.navigateUp()
     }
 
-    fun openAboutFragment(activity: AppCompatActivity) {
-        val navigation = Navigation.findNavController(activity, R.id.mainControllerContext)
-        navigation.navigate(R.id.aboutPage)
+    fun openAboutFragment(navController: NavController) {
+        navController.navigate(R.id.aboutPage)
     }
 }
