f14cc204a0f84b68a0fc2513f6e90078fbadcf08
Ivan <chek539camo@gmail.com>
2021-04-25 22:15:10 +0500

    Added habitElementDao for working with database with methods IHabitElementDao.

--- app/src/main/java/com/example/learnkotlin/repositories/HabitElementRepository.kt
+++ app/src/main/java/com/example/learnkotlin/repositories/HabitElementRepository.kt
@@ -1,12 +1,48 @@
 package com.example.learnkotlin.repositories
 
-import androidx.lifecycle.MutableLiveData
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.example.learnkotlin.HabitTrackerDatabase
+import com.example.learnkotlin.interfaces.IHabitElementDao
 import com.example.learnkotlin.models.HabitElement
 
-class HabitElementRepository {
+class HabitElementRepository(application: Application) : IHabitElementDao {
     companion object {
-        val instance: HabitElementRepository = HabitElementRepository()
+        private var instance: HabitElementRepository? = null
+
+        fun getInstance(application: Application): HabitElementRepository {
+            instance?.let {
+                return it
+            }
+
+            instance = HabitElementRepository(application)
+
+            return instance as HabitElementRepository
+        }
     }
 
-    val habitElements = MutableLiveData<ArrayList<HabitElement>>().apply { value = ArrayList() }
+    private val habitElementDao: IHabitElementDao =
+        HabitTrackerDatabase.getInstance(application).habitElementDao()
+
+    val habitElements: LiveData<List<HabitElement>> = habitElementDao.getAll()
+
+    override fun getAll(): LiveData<List<HabitElement>> {
+        return habitElementDao.getAll()
+    }
+
+    override fun insert(habitElement: HabitElement) {
+        habitElementDao.insert(habitElement)
+    }
+
+    override fun update(habitElement: HabitElement) {
+        habitElementDao.update(habitElement)
+    }
+
+    override fun delete(habitElement: HabitElement) {
+        habitElementDao.delete(habitElement)
+    }
+
+    override fun deleteAll() {
+        habitElementDao.deleteAll()
+    }
 }
