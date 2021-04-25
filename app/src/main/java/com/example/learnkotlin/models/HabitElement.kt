210086092c55ddb01ef382b950b84fe79e54c19f
Ivan <chek539camo@gmail.com>
2021-04-25 22:16:30 +0500

    Made HabitElement.kt Entity. Created TypeConverters for enums.

--- app/src/main/java/com/example/learnkotlin/models/HabitElement.kt
+++ app/src/main/java/com/example/learnkotlin/models/HabitElement.kt
@@ -1,17 +1,47 @@
 package com.example.learnkotlin.models
 
 import android.os.Parcelable
+import androidx.room.*
 import com.example.learnkotlin.enums.HabitType
 import com.example.learnkotlin.enums.PriorityType
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
+@TypeConverters(PriorityConverter::class, HabitTypeConverter::class)
+@Entity(tableName = "habit_table")
 data class HabitElement(
-    var title: String,
+    @ColumnInfo var title: String,
     var description: String,
     var priority: PriorityType,
     var type: HabitType,
     var completeCounter: Int,
     var periodNumber: String,
     var color: String
-) : Parcelable
+) : Parcelable {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+}
+
+class PriorityConverter {
+    @TypeConverter
+    fun fromPriority(priority: PriorityType): String {
+        return priority.stringValue
+    }
+
+    @TypeConverter
+    fun toPriority(data: String): PriorityType {
+        return PriorityType.fromString(data)
+    }
+}
+
+class HabitTypeConverter {
+    @TypeConverter
+    fun fromHabitType(habitType: HabitType): String {
+        return habitType.typeString
+    }
+
+    @TypeConverter
+    fun toHabitType(data: String): HabitType {
+        return HabitType.fromString(data)
+    }
+}
