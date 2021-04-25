2e1ac6906fb271f6b379afcbf6f91bfbdf60ea79
Ivan <chek539camo@gmail.com>
2021-04-25 22:10:47 +0500

    Created HabitTrackerDatabase.kt

---
+++ app/src/main/java/com/example/learnkotlin/HabitTrackerDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.learnkotlin
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.learnkotlin.interfaces.IHabitElementDao
+import com.example.learnkotlin.models.HabitElement
+
+@Database(entities = [HabitElement::class], version = 1)
+abstract class HabitTrackerDatabase : RoomDatabase() {
+    companion object {
+        private var instance: HabitTrackerDatabase? = null
+
+        fun getInstance(context: Context): HabitTrackerDatabase {
+            instance?.let {
+                return it
+            }
+
+            instance = Room.databaseBuilder(
+                context,
+                HabitTrackerDatabase::class.java,
+                "habit_tracker_database"
+            ).allowMainThreadQueries().build()
+
+            return instance as HabitTrackerDatabase
+        }
+    }
+
+    abstract fun habitElementDao(): IHabitElementDao
+}
