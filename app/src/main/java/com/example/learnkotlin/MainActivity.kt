820c536647941fe863f214e6c7decc1dc68f93d8
Ivan <chek539camo@gmail.com>
2021-05-13 00:22:52 +0500

    Added glide implemented at build.gradle Added navigation_header.xml with avatar at navigation_header.xml. Added upload avatar from url at MainActivity.kt.

--- app/src/main/java/com/example/learnkotlin/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.learnkotlin
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
@@ -8,11 +9,13 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import com.bumptech.glide.Glide
 import com.example.learnkotlin.databinding.ActivityMainBinding
 
 
 const val ARGS_HABIT_ELEMENTS = "habitElements"
 const val ARGS_HABIT_ELEMENT = "habitElement"
+const val TOKEN = "c95d0e92-1c58-40b6-9df5-6ed542cb8233"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -29,6 +32,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun changeActivity() {
         setSupportActionBar(binding.toolbar)
+
+        val headerLayout = binding.navigationView.getHeaderView(0)
+        val avatarImage = headerLayout.findViewById<ImageView>(R.id.avatar)
+        Glide.with(this)
+            .load("https://avatarfiles.alphacoders.com/274/274303.png")
+            .placeholder(R.drawable.ahe_launcher_round)
+            .error(R.drawable.error_image_round)
+            .circleCrop()
+            .into(avatarImage)
 
         appBarConfiguration = AppBarConfiguration(
             setOf(R.id.homePage, R.id.aboutPage), binding.drawerLayout
