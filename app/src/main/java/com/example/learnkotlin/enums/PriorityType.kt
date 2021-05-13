1af6039ff55c2a38f92deef1a62c7cba0ce9c3de
Ivan <chek539camo@gmail.com>
2021-05-13 17:23:54 +0500

    Deleted "Very high" priority.

--- app/src/main/java/com/example/learnkotlin/enums/PriorityType.kt
+++ app/src/main/java/com/example/learnkotlin/enums/PriorityType.kt
@@ -3,8 +3,7 @@ package com.example.learnkotlin.enums
 enum class PriorityType(val priorityId: Int, val stringValue: String) {
     Low(0, "Low"),
     Medium(1, "Medium"),
-    High(2, "High"),
-    VeryHigh(3, "Very high");
+    High(2, "High");
 
     companion object {
         fun fromString(value: String) = values().first { it.stringValue == value }
